7b845e3bb4a5bc4d581cd4d1ebebd474664f33d5
Oki <104417609+OktayBogazkaya@users.noreply.github.com>
2024-08-27 13:38:44 +0200

    Add date and author extraction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-
 import time
-import re
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -9,9 +7,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
+
+author = "rhinoinsight" #Name of the Substack you want to scrap
+substack_url = f"https://{author}.substack.com/archive"
+
 # Step 1: Set up the Selenium WebDriver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://rhinoinsight.substack.com/archive") #URL of the Substack you want to scrap
+driver.get(substack_url)
 
 # Step 2: Scroll to the bottom to load more posts
 last_height = driver.execute_script("return document.body.scrollHeight")
@@ -42,20 +44,28 @@ if archive_list:
     # Extract all the titles, subtitles, and links
     title_elements = archive_list.find_all('a', attrs={"data-testid": "post-preview-title"})
     subtitle_elements = archive_list.find_all('a', class_="_color-primary_3axfk_183")
+    datetime_elements = archive_list.find_all('time', class_="_date_1v6nm_1")
 
-    for title_elem, subtitle_elem in zip(title_elements, subtitle_elements):
+    for title_elem, subtitle_elem, date_elem in zip(title_elements, subtitle_elements, datetime_elements):
         title = title_elem.text.strip()
         subtitle = subtitle_elem.text.strip()
         link = title_elem['href']
+        date_disp = date_elem.text.strip()
+        date = date_elem.get('datetime')
+      
 
         # Append the extracted data to the articles list
         articles.append({
             "Title": title,
             "Subtitle": subtitle,
-            "Link": link
+            "Link": link,
+            "Author": author,
+            "Date": date,
+            "Date Displayed": date_disp
+
         })
 
 # Step 5: Store the data in an Excel file
 df = pd.DataFrame(articles)
-df.to_excel("substack_articles_with_links.xlsx", index=False, engine='openpyxl')
-print("Data successfully written to substack_articles_with_links.xlsx")
+df.to_excel(f"{author}_substack_articles_with_links.xlsx", index=False, engine='openpyxl')
+print("Data successfully written")
